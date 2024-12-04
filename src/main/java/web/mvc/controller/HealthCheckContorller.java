f6bb1f0bb0da4007ae0113e5265fce8df3ff7206
hoseok <swiftie52@naver.com>
2024-12-04 09:55:21 +0900

    fix : 주석 추가

--- src/main/java/web/mvc/controller/HealthCheckContorller.java
+++ src/main/java/web/mvc/controller/HealthCheckContorller.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// aws als 헬스체크용
 @RestController
 public class HealthCheckContorller {
     @GetMapping("/check")
